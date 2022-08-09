cb6f24bd50d71963e7b8c8264754abec600f5ef3
BaurinAnton <baurinanton2013@yandex.ru>
2022-08-09 21:36:01 +0300

    feat(app): weba production;

---
+++ weBa/modules/home/layouts/Portfolio/Tablet/constants.js
@@ -0,0 +1,11 @@
+export const PORTFOLIO = {
+    text: 'Портфолио',
+    work: [
+        {id: 1, content: 'Автогруз', source: '/img/home/Portfolio/workOne.jpg', href: 'https://avtogruz62.ru', alt: 'avtogruz62'},
+        {id: 2, content: 'Шерифф', source: '/img/home/Portfolio/workFour.jpg', href: 'https://baurinanton.github.io/sherif', alt: 'sheriff'},
+        {id: 3, content: 'Добрый пар', source: '/img/home/Portfolio/workTo.jpg', href: 'http://good-par.ru', alt: 'good par'},
+        {id: 4, content: 'Автопомощь', source: '/img/home/Portfolio/workThree.jpg', href: 'http://autohelp50.ru', alt: 'autohelp50'},
+        {id: 5, content: '#ТЫЖПРОГРАММИСТ', source: '/img/home/Portfolio/workFive.jpg', href: 'https://baurinanton.github.io/typro', alt: 'typro'},
+    ],
+    button: 'Увидеть больше.',
+}
